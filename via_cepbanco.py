ed52a65f99149ac92dfbfc492fe09625f6aea03b
Caio Marinho <108732958+Caio-Marinho@users.noreply.github.com>
2024-11-05 23:00:48 +0000

    semi_automação

--- via_cepbanco.py
+++ via_cepbanco.py
@@ -1,23 +1,29 @@
 import mysql.connector
 import requests
 lista = []
+chaves = []
 banco = mysql.connector.connect(
-    host="localhost",
+    host="0.tcp.sa.ngrok.io",
+    port = 17698,
     user="root",
-    password="123456",
-    database= "turmaa"
+    password="teste",
+    database= "cdd40"
 )
-cep = input("Digite o cep: ")
+cep = '50810020'#input("Digite o cep: ")
 if len(cep) == 8:
     link = f"https://viacep.com.br/ws/{cep}/json/"
     requisicao = requests.get(link)
     dic_requisicao = requisicao.json()
     print(dic_requisicao)
     for i in dic_requisicao:
+        chaves.append(i)
         lista.append(dic_requisicao[i])
+    espaco = ', '.join(["%s"]*len(dic_requisicao))
+    titulo = ', '.join(chaves)
     meucursor = banco.cursor()
-    sql = ("INSERT INTO endereco (cep,lougradouro,complemento,unidade,bairro,localidade,uf,estado,regiao,ibge,gia,ddd,siafi)"
-           "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);")
+    sql = (f"INSERT INTO endereco ({titulo})"
+           f"VALUES ({espaco});")
+    lista[0] = lista[0][:5]+lista[0][6:]
     dados = tuple(lista)
     meucursor.execute(sql,dados)
     banco.commit()
